701d92954a8888811a67800fb262ae988511de12
wakidurrahman <wakidur.rahman@bjitgroup.com>
2021-09-15 12:01:10 +0600

    add screma

--- products/product-model.js
+++ products/product-model.js
@@ -1,5 +1,105 @@
-// first commit
-console.log('Product');
-console.log("how are you");
-console.log("how are you");
-console.log("how are you");
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema(
+  {
+    businessId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Business',
+      required: true,
+    },
+    creatorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'Category is required'],
+      ref: 'ProductCategory',
+    },
+    name: {
+      type: String,
+      required: [true, 'Product Name is required'],
+      index: true,
+      unique: true,
+      dropDups: true,
+      trim: true,
+    },
+    productImage: {
+      type: String,
+      required: [true, 'Image is required'],
+    },
+    brand: {
+      type: String,
+      required: [true, 'Brand is required'],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Price is required'],
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      default: 0,
+      required: [true, 'Product Stock is required'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+    },
+    averageRating: {
+      type: Number,
+      default: 1,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10, // 4.666666, 46.6666, 47, 4.7
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    discountRate: {
+      type: Number,
+      required: [false, 'Discount Rate is required'],
+      default: 0,
+    },
+    priceDiscount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    priceAfterDiscount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    shippingCharge: {
+      type: Number,
+      required: [true, 'Shipping Charge is required'],
+      default: 0,
+    },
+    taxRate: {
+      type: Number,
+      required: [true, 'Please add a Tax Rate'],
+      default: 0,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
--- users/user-model.js
+++ users/user-model.js
@@ -1,4 +1,175 @@
-// First commit
-console.log('First');
-console.log('Hello User');
-console.log('User update')
+
+ /**
+  * 3rd party modules from npm.
+  */
+  const mongoose = require('mongoose');
+
+  const UserSchema = new mongoose.Schema(
+    {
+      userType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserType',
+        required: true,
+      },
+      userRole: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserRole',
+        required: true,
+      },
+      userStatus: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserStatus',
+        required: true,
+      },
+      businessId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Business',
+        required: false,
+      },
+      businessType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'BusinessType',
+        required: false,
+      },
+      firstName: {
+        type: String,
+        required: false,
+      },
+      lastName: {
+        type: String,
+        required: false,
+      },
+      email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        index: true,
+        unique: true,
+        dropDups: true,
+        lowercase: true,
+        // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
+        match: [
+          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          'Please enter a valid email',
+        ],
+      },
+      emailVerificationToken: {
+        type: String,
+        required: false,
+      },
+      emailVerified: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+      password: {
+        type: String,
+        required: [true, "Password can't be empty"],
+        minlength: [6, 'Password must be atleast 6 character long'],
+        select: false,
+      },
+      passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+          // This only works on CREATE and SAVE!!!
+          validator: function (el) {
+            return el === this.password;
+          },
+          message: 'Passwords are not the same!',
+        },
+      },
+      passwordChangedAt: {
+        type: Date,
+        required: false,
+      },
+      passwordResetToken: {
+        type: String,
+        required: false,
+      },
+      passwordResetExpires: {
+        type: Date,
+        required: false,
+      },
+      saltSecret: {
+        type: String,
+        required: false,
+        select: false,
+      },
+      photo: {
+        type: String,
+        required: false,
+      },
+      designation: {
+        type: String,
+        required: false,
+      },
+      address: {
+        type: String,
+        required: false,
+      },
+      phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters'],
+        required: false,
+      },
+      dateOfBirth: {
+        type: Date,
+        required: false,
+      },
+      gender: {
+        type: String,
+        enum: ['Male', 'Female'],
+        required: false,
+      },
+      termsAndConditions: {
+        type: Boolean,
+        required: false,
+        default: false,
+        select: false,
+      },
+      minValidJWTIat: {
+        type: Date,
+        required: false,
+      },
+      website: {
+        type: String,
+        match: [
+          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+          'Please use a valid URL with HTTP or HTTPS',
+        ],
+      },
+      occupation: {
+        type: String,
+        required: false,
+      },
+      religious: {
+        type: String,
+        required: false,
+      },
+      active: {
+        type: Boolean,
+        default: true,
+        select: false,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false,
+      },
+      updatedAt: {
+        type: Date,
+        default: Date.now(),
+        select: false,
+      },
+    },
+    {
+      timestamps: true,
+      versionKey: false,
+      toJSON: { virtuals: true },
+      toObject: { virtuals: true },
+    }
+  );
+ 
+ 
+  // Export function to create "SomeModel" model class
+  module.exports = mongoose.model('User', UserSchema);
